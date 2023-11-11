fb7dfe831f85d4077cdf534f36cbefdbc8355461
Davi Ribeiro <davi.rb@hotmail.com>
2023-11-11 17:33:49 -0300

    construindo a estrutura e a seção sobre mim

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Teste from './Teste'
-import EstiloGlobal from './styles'
+import Projetos from './components/Projetos'
+import Sobre from './components/Sobre'
+import Sidebar from './containers/Sidebar'
+import EstiloGlobal, { Container } from './styles'
 
 function App() {
   return (
     <>
       <EstiloGlobal />
-      <Teste />
+      <Container>
+        <Sidebar />
+        <main>
+          <Sobre />
+          <Projetos />
+        </main>
+      </Container>
     </>
   )
 }
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/daviirb.png" alt="" />
+    <Titulo fontSize={20}>Davi Ribeiro</Titulo>
+  </aside>
+)
+
+export default Sidebar
